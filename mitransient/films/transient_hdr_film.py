5adb810df8dedbf8f5f5a1b931a2e6d0dbd8bd04
Jorge Solan Morote <816259@unizar.es>
2024-05-16 13:07:23 +0200

    Modificacion de los xml y pruebas con nlos

--- mitransient/films/transient_hdr_film.py
+++ mitransient/films/transient_hdr_film.py
@@ -1,40 +1,33 @@
 import mitsuba as mi
 import drjit as dr
 
-import csv
+from mitransient.utils import speed_of_light
 import numpy as np
 from mitsuba import is_monochromatic, is_spectral
 from mitransient.render.transient_block import TransientBlock
 
-def leer_spad():
+def leer_spad(path, volt):
     times = dr.zeros(mi.Float, 4096)
-    pdf_sub20 = dr.zeros(mi.Float, 4096)
-    pdf_sub5 = dr.zeros(mi.Float, 4096)
+    pdf = dr.zeros(mi.Float, 4096)
 
-    with open('../20um_cmos.csv', 'r') as f:
+    with open(path, 'r') as f:
         f.readline()
         for i in range(4096): # 4096 valid rows
             t, vsub20, vsub5 = f.readline().split(',')
             times[i] = float(t)
-            pdf_sub20[i] = float(vsub20)
-            pdf_sub5[i] = float(vsub5)
+            pdf[i] = float(vsub20) if volt == 20 else float(vsub5) if volt == 5 else 0.0
 
     # normalize PDFs
-    total_sub20 = sum(pdf_sub20)
-    total_sub5 = sum(pdf_sub5)
-    pdf_sub20 = [x / total_sub20 for x in pdf_sub20]
-    pdf_sub5 = [x / total_sub5 for x in pdf_sub5]
+    total = sum(pdf)
+    pdf = [x / total for x in pdf]
 
     # calculate CDFs
-    cdf_sub20 = [0.0] * 4096
-    cdf_sub5 = [0.0] * 4096
-    cdf_sub20[0] = pdf_sub20[0]
-    cdf_sub5[0] = pdf_sub5[0]
+    cdf = [0.0] * 4096
+    cdf[0] = pdf[0]
     for i in range(1, 4096):
-        cdf_sub20[i] = cdf_sub20[i-1] + pdf_sub20[i]
-        cdf_sub5[i] = cdf_sub5[i-1] + pdf_sub5[i]
+        cdf[i] = cdf[i-1] + pdf[i]
 
-    return cdf_sub5, cdf_sub20
+    return times, cdf
 
 class TransientHDRFilm(mi.Film):
     """
@@ -68,16 +61,21 @@ class TransientHDRFilm(mi.Film):
     def __init__(self, props):
         super().__init__(props)
         
-        # Llamada a la función
-        self.cdf_5, self.cdf_20 = leer_spad()
-
-        self.disc_5 = mi.DiscreteDistribution(self.cdf_5)
-        self.disc_20 = mi.DiscreteDistribution(self.cdf_20)
-
         # NOTE: Also inherits properties from mi.Film (see documentation for this class above)
+        self.path = props.get("spad_csv", "")
+        self.mod_spad = props.get("mod_spad", 1)
         self.temporal_bins = props.get("temporal_bins", mi.UInt32(2048))
         self.bin_width_opl = props.get("bin_width_opl", mi.Float(0.003))
         self.start_opl = props.get("start_opl", mi.UInt32(0))
+        self.use_spad = props.get("use_spad", True)
+        self.spad_volt = props.get("spad_volt", 20)
+        self.spad_lost = props.get("spad_lost", 0)
+
+        # Llamada a la función
+        if self.use_spad:
+            self.time, self.cdf, = leer_spad(self.path, self.spad_volt)
+            self.time = mi.Float(self.time)
+            self.disc = mi.DiscreteDistribution(self.cdf)
 
         dr.make_opaque(self.temporal_bins, self.bin_width_opl, self.start_opl)
 
@@ -95,20 +93,21 @@ class TransientHDRFilm(mi.Film):
         * pos: pixel position
         * ray_weight: weight of the ray given by the sensor
         """
- 
-        sample2 = sampler.next_2d()
-        index, new_sample,shape_pdf = self.disc_5.sample_reuse_pmf(sample2[0], active)
-
-        dr.printf_async("index: %i\n", index)
-
-        result = dr.gather(mi.Float,self.cdf_5,index)
-        dr.printf_async("result: %f\n", result)
-
-        #jitter_index, new_sample = a.sample_reuse(s, active)
+        if self.use_spad:
+            sample2 = sampler.next_2d() # Números aleatorios
+            mask_spad = sample2[1] < self.spad_lost # Spad detecta el 30% de los fotones que le llegan
+            index, _ = self.disc.sample_reuse(sample2[0], active)
+            
+            result = dr.gather(dtype=type(self.time), source=self.time, index=index)
+            dist_spad = result * speed_of_light * self.mod_spad
+        else:
+            mask_spad = True
+            dist_spad = 0.0
 
         idd = (distance - self.start_opl) / self.bin_width_opl
-        coords = mi.Vector3f(pos.x, pos.y, idd)
-        mask = (idd >= 0) & (idd < self.temporal_bins)
+
+        coords = mi.Vector3f(pos.x, pos.y, idd + dist_spad)
+        mask = (idd >= 0) & (idd < self.temporal_bins) & mask_spad
         self.transient.put(
             pos=coords,
             wavelengths=wavelengths,
